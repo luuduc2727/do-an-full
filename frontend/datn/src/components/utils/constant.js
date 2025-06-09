8115f49725d2cc926c0c9b8246b867e2dfc41ac1
Lưu Anh Đức <luuanhduc@MacBook-Air-cua-Luu.local>
2025-06-09 10:28:29 +0700

    fix api

--- frontend/datn/src/components/utils/constant.js
+++ frontend/datn/src/components/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user"
-export const JOB_API_URL_ENDPOINT = "http://localhost:8000/api/v1/job"
-export const COMPANY_API_URL_ENDPOINT = "http://localhost:8000/api/v1/company"
-export const APPLICATION_API_URL_ENDPOINT = "http://localhost:8000/api/v1/application"
+export const USER_API_END_POINT = "https://do-an-backend-fc47.onrender.com/api/v1/user"
+export const JOB_API_URL_ENDPOINT = "https://do-an-backend-fc47.onrender.com/api/v1/job"
+export const COMPANY_API_URL_ENDPOINT = "https://do-an-backend-fc47.onrender.com/api/v1/company"
+export const APPLICATION_API_URL_ENDPOINT = "https://do-an-backend-fc47.onrender.com/api/v1/application"
 
